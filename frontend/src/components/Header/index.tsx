5f825c6dffe35a8ffff3adbcf8e28bd02a227d89
LENOVO-PC\Robson <romelo.br@gmail.com>
2022-09-07 18:43:37 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>Desnvolvido por
+                        <a href="https://www.linkedin.com/in/robson-andrade-melo-2019/">robsonmelo@linkedin</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
